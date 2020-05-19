a20d38ebb072b963fcdec193a87eeaaf20a6b302
Chris Trevino <chtrevin@microsoft.com>
2020-05-19 15:53:57 -0700

    fix: lint issue

---
+++ packages/scripts/config/licenses-to-fail-config.js
@@ -0,0 +1,25 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+module.exports = {
+	allowedLicenses: [
+		'MIT',
+		'ISC',
+		'Apache 2',
+		'Apache-2.0',
+		'BSD',
+		'BSD-2-Clause',
+		'BSD-3-Clause',
+		'0BSD',
+		'MPL-2.0',
+		'Unlicense',
+		// Art & Creative-Commons Licenses
+		'Artistic-2.0',
+		'CC0-1.0',
+		'CC-BY-2.0',
+		'CC-BY-3.0',
+		'CC-BY-4.0',
+	],
+	warnOnUnknown: true,
+}
