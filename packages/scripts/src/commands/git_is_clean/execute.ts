14a7435bb0924b7807bd54f100fbc6a9c756b746
Chris Trevino <chtrevin@microsoft.com>
2020-06-26 11:09:22 -0700

    build: updates

--- packages/scripts/src/commands/git_is_clean/execute.ts
+++ packages/scripts/src/commands/git_is_clean/execute.ts
@@ -4,6 +4,9 @@
  */
 import { runSequential } from '../../utils'
 
+/**
+ * Verifies that there are no active changes in the git index
+ */
 export async function execute(): Promise<number> {
 	return runSequential([
 		{
