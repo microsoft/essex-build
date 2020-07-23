ec74b1a2d46258622d71bda2f053d759038fc6cc
Chris Trevino <chtrevin@microsoft.com>
2020-07-23 15:48:57 -0700

    build: update jest configuration

--- packages/config/jest-config/src/getDefaultConfiguration.ts
+++ packages/config/jest-config/src/getDefaultConfiguration.ts
@@ -2,30 +2,19 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import { join } from 'path'
-import { defaults } from 'jest-config'
 import { getSetupFiles, getTsConfigFile } from './overrides'
 
 export function getDefaultConfiguration(): any {
-	const emptyMock = join(__dirname, 'filemock.js')
-
 	return {
 		preset: 'ts-jest',
 		transform: {
 			'^.+\\.[tj]sx?$': 'ts-jest',
 		},
 		testMatch: ['**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)'],
-		moduleFileExtensions: [
-			...defaults.moduleFileExtensions,
-			'ts',
-			'tsx',
-			'json',
-		],
-		testEnvironment: 'jsdom',
-		rootDir: process.cwd(),
 		testResultsProcessor: 'jest-junit-reporter',
 		moduleNameMapper: {
-			'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': emptyMock,
+			'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+				'@essex/jest-config/lib/filemock',
 			'\\.(css|less|scss|sass)$': 'identity-obj-proxy',
 		},
 		collectCoverageFrom: [
