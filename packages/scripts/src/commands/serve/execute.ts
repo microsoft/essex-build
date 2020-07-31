ec1e6293e9d297202ece160f15ffe47d4890ce65
Chris Trevino <chtrevin@microsoft.com>
2020-07-31 10:59:44 -0700

    build: move the start_storybook functionality to essex start

--- packages/scripts/src/commands/serve/execute.ts
+++ packages/scripts/src/commands/serve/execute.ts
@@ -2,18 +2,25 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
+import { storybookStart } from '@essex/build-step-storybook'
 import { webpackServe } from '@essex/build-step-webpack'
 
 export interface ServeCommandOptions {
 	mode?: 'production' | 'development'
 	env?: string
 	verbose?: boolean
+	storybook?: boolean
 }
 
 export async function execute({
 	env = 'development',
 	mode = 'development',
-	verbose,
+	verbose = false,
+	storybook,
 }: ServeCommandOptions): Promise<void> {
-	return webpackServe({ env, mode, verbose })
+	if (storybook) {
+		return storybookStart(verbose)
+	} else {
+		return webpackServe({ env, mode, verbose })
+	}
 }
