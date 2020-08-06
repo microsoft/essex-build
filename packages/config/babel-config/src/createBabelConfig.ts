3f2c29b4c3ce08848a6730fd37becbf07abd88fa
Chris Trevino <darthtrevino@users.noreply.github.com>
2020-08-05 19:57:13 -0700

    Fix Babel Config (#127)
    * fix: restore some babel plugins for webpack 4 interop
    
    * build: upgrade dependencies
    
    * v9.0.3
    
    * build: update yarn lock

--- packages/config/babel-config/src/createBabelConfig.ts
+++ packages/config/babel-config/src/createBabelConfig.ts
@@ -20,5 +20,11 @@ export function createBabelConfig(
 				},
 			],
 		],
+		plugins: [
+			require('@babel/plugin-proposal-class-properties'),
+			require('@babel/plugin-proposal-object-rest-spread'),
+			require('@babel/plugin-proposal-optional-chaining'),
+			require('@babel/plugin-proposal-nullish-coalescing-operator'),
+		],
 	} as any
 }
