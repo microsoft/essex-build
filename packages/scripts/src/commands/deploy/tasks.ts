81f3fb23f01271f93edfe1494b0c9f4fd2ea0663
Chris Trevino <darthtrevino@users.noreply.github.com>
2020-08-10 17:47:57 -0700

    Update deploy args (#130)
    * feat: throw errors when required arg are not passed in deploy
    
    * build: add args
    
    * v9.1.1
    
    * build: update yarn.lock

--- packages/scripts/src/commands/deploy/tasks.ts
+++ packages/scripts/src/commands/deploy/tasks.ts
@@ -6,17 +6,28 @@ import { deployBlob } from '@essex/build-step-azure'
 import { DeployCommandOptions, DeployType } from './types'
 
 export function execute(options: DeployCommandOptions): Promise<void> {
+	if (!options.type) {
+		throw new Error("'type' argument not set")
+	}
 	switch (options.type) {
 		case DeployType.AzureBlobStorage: {
 			return executeAzureBlobDeploy(options)
 		}
+		default:
+			throw new Error(`Unhandled deploy type: ${options.type}`)
 	}
 }
 
-function executeAzureBlobDeploy(options: DeployCommandOptions) {
-	return deployBlob(
-		options.storageAccount,
-		options.storageAccountKey,
-		options.verbose,
-	)
+function executeAzureBlobDeploy({
+	storageAccount,
+	storageAccountKey,
+	verbose = false,
+}: DeployCommandOptions) {
+	if (!storageAccount) {
+		throw new Error('storageAccount must be set')
+	}
+	if (!storageAccountKey) {
+		throw new Error('storageAccountKey must be set')
+	}
+	return deployBlob(storageAccount, storageAccountKey, verbose)
 }
