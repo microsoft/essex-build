9a45d2b4601f92b1dea9e65a18057805ec7e175e
Chris Trevino <chtrevin@microsoft.com>
2020-10-06 08:47:04 -0700

    build: try using just

--- packages/tasks/src/index.ts
+++ packages/tasks/src/index.ts
@@ -15,84 +15,87 @@ import { clean as cleanTask } from '@essex/build-step-clean'
 import { jestGulp } from '../../scripts/node_modules/@essex/build-step-jest/lib'
 import { deployTask } from './deploy'
 
-option('verbose')
-option('stripInternalTypes')
-option('env')
-option('mode')
-option('docs')
-option('coverage')
-option('ci')
-option('storybook')
-option('webpack')
-option('rollup')
-option('cleanFiles')
-option('jestClearCache')
-option('jestUpdateSnapshots')
-option('jestCoverageThreshold')
-option('jestBrowser')
-option('jestConfigFile')
-option('deployStorageAccount')
-option('deployStorageAccountKey')
-option('deployType')
+export function preset() {
+	option('verbose', { boolean: true })
+	option('stripInternalTypes', { boolean: true })
+	option('env', { string: true, default: 'production' as any })
+	option('mode', { string: true, default: 'production' as any })
+	option('docs', { boolean: true })
+	option('coverage', { boolean: true })
+	option('ci', { boolean: true })
+	option('storybook', { boolean: true })
+	option('webpack', { boolean: true })
+	option('rollup', { boolean: true })
+	option('cleanFiles', { boolean: true })
+	option('jestClearCache', { boolean: true })
+	option('jestUpdateSnapshots', { boolean: true })
+	option('jestCoverageThreshold', { number: true })
+	option('jestBrowser', { boolean: true })
+	option('jestConfigFile', { string: true })
+	option('deployStorageAccount', { string: true })
+	option('deployStorageAccountKey', { string: true })
+	option('deployType', { string: true })
 
-task('typedocs', () => generateTypedocsGulp(argv()['verbose']))
-task('typings', () => emitTypings(argv()['stripInternalTypes']))
-task('tsc', compileTypescript())
-task('babel', buildBabel(argv()['env']))
+	console.log('A', task)
+	task('typedocs', () => generateTypedocsGulp(argv()['verbose']))
+	// task('typings', () => emitTypings(argv()['stripInternalTypes']))
+	// task('tsc', compileTypescript())
+	// task('babel', buildBabel(argv()['env']))
 
-task('build', () =>
-	parallel(
-		condition('typedocs', argv()['docs']),
-		'tyings',
-		series('tsc', 'babel'),
-	),
-)
+	// task('build', () =>
+	// 	parallel(
+	// 		condition('typedocs', argv()['docs']),
+	// 		'tyings',
+	// 		series('tsc', 'babel'),
+	// 	),
+	// )
 
-task('test', () =>
-	jestGulp({
-		verbose: argv()['verbose'],
-		coverage: argv()['coverage'],
-		watch: argv()['watch'],
-		ci: argv()['ci'],
-		clearCache: argv()['jestClearCache'],
-		updateSnapshots: argv()['jestUpdateSnapshots'],
-		coverageThreshold: argv()['jestCoverageThreshold'],
-		browser: argv()['jestBrowser'],
-		configFile: argv()['jestConfigFile'],
-	}),
-)
+	// task('test', () =>
+	// 	jestGulp({
+	// 		verbose: argv()['verbose'],
+	// 		coverage: argv()['coverage'],
+	// 		watch: argv()['watch'],
+	// 		ci: argv()['ci'],
+	// 		clearCache: argv()['jestClearCache'],
+	// 		updateSnapshots: argv()['jestUpdateSnapshots'],
+	// 		coverageThreshold: argv()['jestCoverageThreshold'],
+	// 		browser: argv()['jestBrowser'],
+	// 		configFile: argv()['jestConfigFile'],
+	// 	}),
+	// )
 
-task('audit_security', auditSecurity)
-task('audit_licenses', auditLicenses)
-task('audit', parallel('audit_security', 'audit_licences'))
+	// task('audit_security', auditSecurity)
+	// task('audit_licenses', auditLicenses)
+	// task('audit', parallel('audit_security', 'audit_licenses'))
 
-task('bundle_storybook', storybookBuildGulp())
-task('bundle_webpack', () =>
-	webpackBuildGulp({
-		env: argv()['env'],
-		mode: argv()['mode'],
-		verbose: argv()['verbose'],
-	}),
-)
-task('bundle_rollup', rollupBuild)
-task(
-	'bundle',
-	parallel(
-		condition('bundle_storybook', argv()['storybook']),
-		condition('bundle_webpack', argv()['webpack']),
-		condition('bundle_rollup', argv()['rollup']),
-	),
-)
+	// task('bundle_storybook', storybookBuildGulp)
+	// task('bundle_webpack', () =>
+	// 	webpackBuildGulp({
+	// 		env: argv()['env'],
+	// 		mode: argv()['mode'],
+	// 		verbose: argv()['verbose'],
+	// 	}),
+	// )
+	// task('bundle_rollup', rollupBuild)
+	// task(
+	// 	'bundle',
+	// 	parallel(
+	// 		condition('bundle_storybook', argv()['storybook']),
+	// 		condition('bundle_webpack', argv()['webpack']),
+	// 		condition('bundle_rollup', argv()['rollup']),
+	// 	),
+	// )
 
-task('clean', () => cleanTask(argv()['cleanFiles']()))
+	// task('clean', () => cleanTask(argv()['cleanFiles']()))
 
-task('commit-msg', checkCommitMessage)
+	// task('commit-msg', checkCommitMessage)
 
-task('deploy', () =>
-	deployTask({
-		verbose: argv()['verbose'],
-		type: argv()['deployType'],
-		storageAccount: argv()['deployStorageAccount'],
-		storageAccountKey: argv()['deployStorageAccountKey'],
-	}),
-)
+	// task('deploy', () =>
+	// 	deployTask({
+	// 		verbose: argv()['verbose'],
+	// 		type: argv()['deployType'],
+	// 		storageAccount: argv()['deployStorageAccount'],
+	// 		storageAccountKey: argv()['deployStorageAccountKey'],
+	// 	}),
+	// )
+}
