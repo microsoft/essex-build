7911dba7c66264cc7e8c4692efb331cb2ada05d9
Chris Trevino <darthtrevino@gmail.com>
2020-10-15 18:04:54 -0700

    feat: add pnp webpack plugin to storybook

--- packages/config-storybook/src/main.ts
+++ packages/config-storybook/src/main.ts
@@ -9,6 +9,7 @@ import { getEsmConfiguration } from '@essex/babel-config'
 import { getNodeModulesPaths } from '@essex/build-util-hoister'
 const createCompiler = require('@storybook/addon-docs/mdx-compiler-plugin')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const PnpWebpackPlugin = require('pnp-webpack-plugin')
 
 function getMdxBabelConfiguration() {
 	const config = getEsmConfiguration('development')
@@ -24,64 +25,76 @@ function getMdxBabelConfiguration() {
 
 const nodeModulesPaths = getNodeModulesPaths()
 
-module.exports = {
-	stories: [
-		join(process.cwd(), 'src/index.ts'),
-		join(process.cwd(), '**/*.stories.(ts|tsx|js|jsx|mdx)'),
-		join(process.cwd(), '**/*_stories.(ts|tsx|js|jsx|mdx)'),
-	],
-	addons: [
-		require.resolve('@storybook/addon-actions/register'),
-		require.resolve('@storybook/addon-links/register'),
-		require.resolve('@storybook/addon-knobs/register'),
-		require.resolve('@storybook/addon-a11y/register'),
-		require.resolve('@storybook/addon-docs/register'),
-	],
-	webpackFinal: async (config: any) => {
-		config.resolve.extensions.push('.ts', '.tsx')
-		config.resolve.modules.push(...nodeModulesPaths)
-		config.module.rules.push({
-			test: /\.(ts|tsx)$/,
-			use: [
-				{ loader: require.resolve('cache-loader') },
-				{
-					loader: require.resolve('babel-loader'),
-					options: getEsmConfiguration('development'),
-				},
-				{
-					loader: require.resolve('ts-loader'),
-					options: {
-						configFile: join(process.cwd(), 'tsconfig.json'),
-						transpileOnly: true,
+export interface Configuration {
+	pnp?: boolean
+}
+
+export function configure({ pnp }: Configuration) {
+	return {
+		stories: [
+			join(process.cwd(), 'src/index.ts'),
+			join(process.cwd(), '**/*.stories.(ts|tsx|js|jsx|mdx)'),
+			join(process.cwd(), '**/*_stories.(ts|tsx|js|jsx|mdx)'),
+		],
+		addons: [
+			require.resolve('@storybook/addon-actions/register'),
+			require.resolve('@storybook/addon-links/register'),
+			require.resolve('@storybook/addon-knobs/register'),
+			require.resolve('@storybook/addon-a11y/register'),
+			require.resolve('@storybook/addon-docs/register'),
+		],
+		webpackFinal: async (config: any) => {
+			if (pnp) {
+				config.resolve.plugins = config.resolve.plugins || []
+				config.resolve.plugins.push(PnpWebpackPlugin)
+				config.resolveLoader.plugins = config.resolveLoader.plugins || []
+				config.resolveLoader.plugins.push(PnpWebpackPlugin.moduleLoader(module))
+			}
+			config.resolve.extensions.push('.ts', '.tsx')
+			config.resolve.modules.push(...nodeModulesPaths)
+			config.module.rules.push({
+				test: /\.(ts|tsx)$/,
+				use: [
+					{ loader: require.resolve('cache-loader') },
+					{
+						loader: require.resolve('babel-loader'),
+						options: getEsmConfiguration('development'),
 					},
-				},
-				{
-					loader: require.resolve('react-docgen-typescript-loader'),
-				},
-			],
-		})
-		config.module.rules.push({
-			test: /\.(stories|story)\.mdx$/,
-			use: [
-				{
-					loader: require.resolve('babel-loader'),
-					options: getMdxBabelConfiguration(),
-				},
-				{
-					loader: require.resolve('@mdx-js/loader'),
-					options: {
-						compilers: [createCompiler({})],
+					{
+						loader: require.resolve('ts-loader'),
+						options: {
+							configFile: join(process.cwd(), 'tsconfig.json'),
+							transpileOnly: true,
+						},
 					},
-				},
-			],
-		})
-		config.module.rules.push({
-			test: /(\.|_)(stories|story)\.[tj]sx?$/,
-			loader: require.resolve('@storybook/source-loader'),
-			exclude: [/node_modules/],
-			enforce: 'pre',
-		})
-		config.plugins.push(new ForkTsCheckerWebpackPlugin())
-		return config
-	},
+					{
+						loader: require.resolve('react-docgen-typescript-loader'),
+					},
+				],
+			})
+			config.module.rules.push({
+				test: /\.(stories|story)\.mdx$/,
+				use: [
+					{
+						loader: require.resolve('babel-loader'),
+						options: getMdxBabelConfiguration(),
+					},
+					{
+						loader: require.resolve('@mdx-js/loader'),
+						options: {
+							compilers: [createCompiler({})],
+						},
+					},
+				],
+			})
+			config.module.rules.push({
+				test: /(\.|_)(stories|story)\.[tj]sx?$/,
+				loader: require.resolve('@storybook/source-loader'),
+				exclude: [/node_modules/],
+				enforce: 'pre',
+			})
+			config.plugins.push(new ForkTsCheckerWebpackPlugin())
+			return config
+		},
+	}
 }
