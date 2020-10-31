2f68b636a83f74982ea2d9c25d1117000a6b6d92
Chris Trevino <chtrevin@microsoft.com>
2020-10-30 17:08:31 -0700

    correct debug import

--- packages/config-webpack/src/log.ts
+++ packages/config-webpack/src/log.ts
@@ -2,5 +2,5 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import debug = require('debug')
+import debug from 'debug'
 export const log = debug('essex:webpack')
