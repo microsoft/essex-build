f45d93b370eac4483c20dcbd591007b9ffdf41ca
Chris Trevino <darthtrevino@users.noreply.github.com>
2020-11-05 13:55:16 -0800

    Config & Library Updates (#217)
    * Update some packages, remove ts-jest
    
    * update default browserslist
    
    * fix browserslist, default webpack hash
    
    * add semver document

--- packages/config-babel/src/getBrowsersList.ts
+++ packages/config-babel/src/getBrowsersList.ts
@@ -7,10 +7,12 @@ export function getBrowsersList(
 	setting: undefined | string[] | Record<string, string[]>,
 ): string | string[] {
 	if (setting == null) {
-		return 'defaults'
+		return DEFAULT_ESSEX_BROWSERSLIST
 	} else if (Array.isArray(setting)) {
 		return setting
 	} else {
 		return setting[env]
 	}
 }
+
+const DEFAULT_ESSEX_BROWSERSLIST = ['>0.5%', 'not IE 11', 'not dead']
