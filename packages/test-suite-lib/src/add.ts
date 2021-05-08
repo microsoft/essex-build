fd26959879c047dbe633cd05e6e34248401120ef
Chris Trevino <chtrevin@microsoft.com>
2021-05-08 14:23:28 -0700

    update lib

--- packages/test-suite-lib/src/add.ts
+++ packages/test-suite-lib/src/add.ts
@@ -2,10 +2,9 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import data from './data.json'
-export * from './letters'
+import dataJson from './data.json'
 
-export const d = data
+export const data = dataJson
 export function add(...nums: number[]): number {
 	return nums.reduce((prev, cur) => prev + cur, 0)
 }
