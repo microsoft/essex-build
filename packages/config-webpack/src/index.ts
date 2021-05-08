d99b5e27add54081d2639e34e1f04814c3f3f756
Chris Trevino <chtrevin@microsoft.com>
2021-05-08 16:11:27 -0700

    add tsconfig paths plugin to webpack stack

--- packages/config-webpack/src/index.ts
+++ packages/config-webpack/src/index.ts
@@ -14,6 +14,7 @@ import {
 	getBabelConfiguration,
 } from './configValues'
 import { log } from './log'
+import TsConfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
 import { validateConfiguration } from './validate'
 
 /* eslint-disable @typescript-eslint/no-var-requires */
@@ -110,6 +111,7 @@ export function configure({
 			alias: {
 				...extendedAliases,
 			},
+			plugins: [new TsConfigPathsPlugin()],
 		},
 		resolveLoader: {
 			modules: [...standardModulePaths, ...extendedResolveLoaderModules],
