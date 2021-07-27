f4eb975c3ed2ccf61809f6d1c5df50b1b890d3b2
Chris Trevino <darthtrevino@users.noreply.github.com>
2021-07-27 14:17:56 -0700

    Add Vite Support (#416)
    * add Vite config; test suite
    
    * update pnp/lockfile
    
    * cut semver
    
    * run linter
    
    * fix lint issues

---
+++ packages/test-suite-webapp-vite/src/main.tsx
@@ -0,0 +1,15 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root'),
+)
