28e3f335d6da965508246eeb4f528e7e8996cd6a
Chris Trevino <chtrevin@microsoft.com>
2021-11-30 11:32:18 -0800

    lint updates

--- packages/test-suite-lib/src/__tests__/add.spec.ts
+++ packages/test-suite-lib/src/__tests__/add.spec.ts
@@ -6,9 +6,9 @@ import { add } from '../add'
 
 describe('the adder', () => {
 	it('can add 2 and 2', () => {
-		expect(add(2, 2)).toEqual(4)
+		expect(add(2, 2)).toBe(4)
 	})
 	it('can add 3 and 5', () => {
-		expect(add(3, 5)).toEqual(8)
+		expect(add(3, 5)).toBe(8)
 	})
 })
