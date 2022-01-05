2898515a4fc69059a12c665d04592be35678d423
Chris Trevino <chtrevin@microsoft.com>
2022-01-05 15:57:49 -0800

    update build script logging

--- packages/scripts/src/steps/typescript/index.ts
+++ packages/scripts/src/steps/typescript/index.ts
@@ -2,9 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import { performance } from 'perf_hooks'
 import ts from 'typescript'
-import { subtaskSuccess, subtaskFail, printPerf } from '../../util/tasklogger'
 import { checkAndEmitTypings } from './checkAndEmitTypings'
 import { compile as compileTS } from './compile'
 import { getSourceFiles } from './getSourceFiles'
@@ -16,14 +14,7 @@ export async function compile(stripInternal: boolean): Promise<void> {
 	if (process.env.ESSEX_DEBUG) {
 		console.log('Using TypeScript version ', ts.version)
 	}
-	const start = performance.now()
-	try {
-		const sourceFiles = await getSourceFiles()
-		await compileTS(sourceFiles, logFiles)
-		await checkAndEmitTypings(sourceFiles, stripInternal)
-		subtaskSuccess(title, printPerf(start))
-	} catch (err) {
-		subtaskFail(`${title} ${printPerf(start)}`)
-		throw err
-	}
+	const sourceFiles = await getSourceFiles()
+	await compileTS(sourceFiles, logFiles)
+	await checkAndEmitTypings(sourceFiles, stripInternal)
 }
