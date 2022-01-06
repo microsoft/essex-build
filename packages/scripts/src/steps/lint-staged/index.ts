98b66911b4bd51dc54664c30e4e1fd15fb25f608
Chris Trevino <chtrevin@microsoft.com>
2022-01-06 10:06:04 -0800

    update lint-staged task to emit error info

--- packages/scripts/src/steps/lint-staged/index.ts
+++ packages/scripts/src/steps/lint-staged/index.ts
@@ -5,17 +5,11 @@
 import { run } from '@essex/shellrunner'
 import { configureJob } from './configure'
 
-export function lintStaged(): Promise<void> {
+export async function lintStaged(): Promise<void> {
 	const job = configureJob()
-	return run(job).then(filterShellCode)
-}
-
-/**
- * A function to be used in .then() chains to convert non-zero exit codes
- * from shellrunner into job failures
- */
-export function filterShellCode({ code }: { code: number }): void {
+	const { code, error, output } = await run(job)
 	if (code !== 0) {
+		console.error(`error running task ${job.exec}`, error, output)
 		throw new Error('non-zero exit code')
 	}
 }
