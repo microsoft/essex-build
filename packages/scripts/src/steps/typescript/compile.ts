874a027a0d31093bd9c155338073c9a5d476efb4
Chris Trevino <chtrevin@microsoft.com>
2022-01-06 09:43:11 -0800

    add swcrc configuration to build script

--- packages/scripts/src/steps/typescript/compile.ts
+++ packages/scripts/src/steps/typescript/compile.ts
@@ -3,23 +3,15 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 /* eslint-disable @essex/adjacent-await */
+import { existsSync, readFileSync } from 'fs'
 import fs from 'fs/promises'
-import path from 'path'
+import path, { join } from 'path'
 import { performance } from 'perf_hooks'
 import * as swc from '@swc/core'
 import { printPerf, subtaskSuccess, traceFile } from '../../util/tasklogger'
 
 const ESM_PATH = 'dist/esm'
 const CJS_PATH = 'dist/cjs'
-
-const ENV_CONFIG: swc.EnvConfig = {
-	coreJs: '3',
-	targets: {
-		node: 14,
-		browsers: ['>0.5%', 'not IE 11', 'not dead'],
-	},
-	mode: 'usage',
-}
 
 export async function compile(
 	fileNames: string[],
@@ -43,24 +35,25 @@ async function transpileFile(filename: string, logFiles: boolean) {
 		traceFile(filename, 'transpile')
 	}
 
+	const options = getSwcOptions()
 	const code = await fs.readFile(filename, { encoding: 'utf8' })
 	const esmResult = writeOutput(code, filename, ESM_PATH, {
+		...options,
 		filename,
-		sourceMaps: true,
 		isModule: true,
-		env: ENV_CONFIG,
 		outputPath: path.dirname(filename).replace(/^src/, CJS_PATH),
 		module: {
+			...(options.module || {}),
 			type: 'es6',
 		},
 	})
 	const cjsResult = writeOutput(code, filename, CJS_PATH, {
+		...options,
 		filename,
-		sourceMaps: true,
 		isModule: true,
-		env: ENV_CONFIG,
 		outputPath: path.dirname(filename).replace(/^src/, CJS_PATH),
 		module: {
+			...(options.module || {}),
 			type: 'commonjs',
 		},
 	})
@@ -84,4 +77,32 @@ function writeOutput(
 			map ? fs.writeFile(mapFile, map, { encoding: 'utf8' }) : null,
 		])
 	})
+}
+
+const DEFAULT_SWC_CONFIG: swc.Config = {
+	sourceMaps: true,
+	env: {
+		coreJs: '3',
+		targets: {
+			node: 14,
+			browsers: ['>0.5%', 'not IE 11', 'not dead'],
+		},
+		mode: 'usage',
+	},
+}
+
+function getSwcOptions() {
+	if (existsSync(join(process.cwd(), '.swcrc'))) {
+		const swcrc = readFileSync(join(process.cwd(), '.swcrc'), 'utf8')
+		const result = JSON.parse(swcrc)
+		if (process.env.ESSEX_DEBUG) {
+			console.log('using custom swc configuration', result)
+		}
+		return result
+	} else {
+		if (process.env.ESSEX_DEBUG) {
+			console.log('using default swc configuration')
+		}
+		return DEFAULT_SWC_CONFIG
+	}
 }
