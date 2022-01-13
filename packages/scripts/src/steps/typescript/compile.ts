9b007f38064654786bfc6ff461c07f877f2b8d64
Chris Trevino <darthtrevino@users.noreply.github.com>
2022-01-12 17:25:20 -0800

    Use react-jsx runtime by default (#510)
    * Use react-jsx runtime by default
    
    * semver
    
    * update tsx handling
    
    * version bump

--- packages/scripts/src/steps/typescript/compile.ts
+++ packages/scripts/src/steps/typescript/compile.ts
@@ -94,6 +94,15 @@ const DEFAULT_SWC_CONFIG: swc.Config = {
 		},
 		mode: 'usage',
 	},
+	jsc: {
+		parser: {
+			syntax: 'typescript',
+			tsx: true,
+		},
+		transform: {
+			react: { runtime: 'automatic', useBuiltins: true },
+		},
+	},
 }
 
 function getSwcOptions() {
