49469fe97e9dd5d5e8a5220111169e69997c835b
Chris Trevino <chtrevin@microsoft.com>
2022-02-17 09:06:25 -0800

    add webpack dev-mode linkage

--- packages/test-lib-legacy/src/letters/index.ts
+++ packages/test-lib-legacy/src/letters/index.ts
@@ -2,4 +2,4 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-export const a = 'a'
+export const c = 'c'
--- packages/test-webpack/src/App.tsx
+++ packages/test-webpack/src/App.tsx
@@ -3,8 +3,18 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 import './index.css'
+import { a } from '@essex/test-lib-dual'
+import { b } from '@essex/test-lib-esm'
+import { c } from '@essex/test-lib-legacy'
 import { memo } from 'react'
 
 export const App: React.FC = memo(function App() {
-	return <div>Hello!!</div>
+	return (
+		<div>
+			<h2>webpack tester</h2>
+			<div>Dual: {a === 'a' ? '✅' : '❌'}</div>
+			<div>ESM:{b === 'b' ? '✅' : '❌'}</div>
+			<div>Legacy: {c === 'c' ? '✅' : '❌'}</div>
+		</div>
+	)
 })
