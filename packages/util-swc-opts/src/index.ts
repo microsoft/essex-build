9a287227d8f9e4d30266a6d6fb4499999c5e78e4
Chris Trevino <chtrevin@microsoft.com>
2022-02-18 12:32:54 -0800

    add swcopts package

---
+++ packages/util-swc-opts/src/index.ts
@@ -0,0 +1,63 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+import { existsSync, readFileSync } from 'fs'
+import path from 'path'
+import type * as swc from '@swc/core'
+import { get, merge } from 'lodash'
+
+const TARGET_PACKAGE_JSON_PATH = path.join(process.cwd(), './package.json')
+const SWCRC_FILE = path.join(process.cwd(), '.swcrc')
+
+const DEFAULT_SWC_CONFIG: swc.Config = {
+	sourceMaps: true,
+	jsc: {
+		target: 'es2020',
+		parser: {
+			syntax: 'typescript',
+			tsx: true,
+			decorators: true,
+			dynamicImport: true,
+		},
+		transform: {
+			react: { runtime: 'automatic', useBuiltins: true },
+		},
+	},
+}
+
+export function getSwcOptions(): swc.Config {
+	if (existsSync(SWCRC_FILE)) {
+		const swcrc = readFileSync(SWCRC_FILE, 'utf8')
+		const result = JSON.parse(swcrc)
+		if (isDebug()) {
+			console.log('using custom swc configuration', result)
+		}
+		return result
+	} else {
+		const pkg = readPackageJson(TARGET_PACKAGE_JSON_PATH)
+		const swcOverrides = get(pkg, 'essex.swc')
+		if (swcOverrides) {
+			const merged = merge(DEFAULT_SWC_CONFIG, swcOverrides)
+			if (isDebug()) {
+				console.log(
+					'applying essex.swc overrides to default swc configuration\n\n',
+					merged,
+				)
+			}
+			return merged
+		} else {
+			if (isDebug()) {
+				console.log('using default swc configuration')
+			}
+			return DEFAULT_SWC_CONFIG
+		}
+	}
+}
+function isDebug() {
+	return Boolean(process.env['ESSEX_DEBUG'])
+}
+function readPackageJson(pkgPath: string) {
+	const data = readFileSync(pkgPath, 'utf-8')
+	return JSON.parse(data)
+}
