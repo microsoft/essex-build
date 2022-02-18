340b831f1b5f8f9cb3b915e180877e28c64fb9ab
Chris Trevino <chtrevin@microsoft.com>
2022-02-18 13:41:37 -0800

    prettier

--- packages/test-lib-esm/src/add.ts
+++ packages/test-lib-esm/src/add.ts
@@ -2,9 +2,8 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-export * from './rendering.js'
-
 import reduce from 'lodash-es/reduceRight.js'
+export * from './rendering.js'
 
 export function add(...nums: number[]): number {
 	return reduce(nums, (prev, curr) => prev + curr, 0)
