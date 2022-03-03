70078784e04c7ff419abb503ed86a1af11512650
Chris Trevino <chtrevin@microsoft.com>
2022-03-03 14:49:11 -0800

    clean up eslint configuration

---
+++ packages/config-eslint-plugin/src/essex/constants.ts
@@ -0,0 +1,8 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+export const TYPESCRIPT_FILES = ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts']
+export const JEST_FILES = ['**/*.spec.*', '**/*.test.*']
+export const REACT_FILES = ['**/*.jsx', '**/*.tsx']
+export const REACT_TS_FILES = ['**/*.tsx']
