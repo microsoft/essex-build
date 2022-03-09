306e34d759e9a4e6ad1e062a51293aeb5d5e30cb
Chris Trevino <chtrevin@microsoft.com>
2022-03-09 11:33:43 -0800

    allow typeaware linting to be overridden via environment variable

--- packages/config-eslint-plugin/src/essex/createConfiguration.ts
+++ packages/config-eslint-plugin/src/essex/createConfiguration.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
+import chalk from 'chalk'
 import type { Linter } from 'eslint'
 
 import {
@@ -14,6 +15,7 @@ import { importSettings, reactSettings } from './settings.js'
 export function createConfiguration(
 	useTypeAwareLinting: boolean,
 ): Linter.Config & { overrides: Linter.ConfigOverride[] } {
+	useTypeAwareLinting = getTypeAwareSetting(useTypeAwareLinting)
 	return {
 		root: true,
 		plugins: [
@@ -51,4 +53,23 @@ export function createConfiguration(
 			jestOverride(),
 		],
 	}
+}
+
+function getTypeAwareSetting(defaultValue: boolean): boolean {
+	if (defaultValue === false) {
+		return false
+	}
+
+	const skipTypecheck = process.env['SKIP_TYPEAWARE_LINTING'] != null
+	if (skipTypecheck) {
+		console.warn(chalk.yellow('skipping type-aware linting'))
+	} else {
+		console.info(
+			chalk.green(
+				'type-aware linting enabled, set SKIP_TYPEAWARE_LINTING=1 to disable',
+			),
+		)
+	}
+
+	return !skipTypecheck
 }
