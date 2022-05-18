80894a1b09f94f9ea3d3d8db55a9f4a193ddbcaa
Chris Trevino <chtrevin@microsoft.com>
2022-05-18 15:09:08 -0700

    lint updates

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { createConfiguration } from './essex/createConfiguration.js'
 import rules from './rules/index.js'
 
--- packages/test-vite/src/main.tsx
+++ packages/test-vite/src/main.tsx
@@ -2,10 +2,12 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
+import './index.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import { App } from './App.js'
-import './index.css'
 
 ReactDOM.render(
 	<React.StrictMode>
