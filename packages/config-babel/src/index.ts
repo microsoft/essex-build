a13d8696c1d2a63f25ae5cbd89775436e3788f42
Chris Trevino <chtrevin@microsoft.com>
2022-05-23 12:12:39 -0700

    linting fixes

--- packages/config-babel/src/index.ts
+++ packages/config-babel/src/index.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { loadJson } from '@essex/babel-config/resolve'
 import { existsSync } from 'fs'
 import { join } from 'path'
