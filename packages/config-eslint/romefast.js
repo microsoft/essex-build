6e3642d327ec66b663ebcbbb4280d2d20477d19e
Chris Trevino <darthtrevino@gmail.com>
2023-01-04 14:52:00 -0800

    update romefast.js to point to the correct plugin preset

--- packages/config-eslint/romefast.js
+++ packages/config-eslint/romefast.js
@@ -3,5 +3,5 @@
  * Licensed under the MIT license. See LICENSE file in the project.
  */
 module.exports = {
-	extends: ['plugin:@essex/eslint-plugin/rome'],
+	extends: ['plugin:@essex/eslint-plugin/romefast'],
 }
