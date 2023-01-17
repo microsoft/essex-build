ca9f8b0a7a4050009fc9bf8a3c38407cff2f38d8
Chris Trevino <darthtrevino@gmail.com>
2023-01-17 10:51:57 -0800

    add @ts-ignore flag to crlf import

--- packages/scripts/src/util/fixLineEndings.ts
+++ packages/scripts/src/util/fixLineEndings.ts
@@ -2,6 +2,8 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
 import crlf from 'crlf'
 import glob from 'glob'
 
