f3f889a4b177812de80eeabde8e0965d8f161e58
Chris Trevino <darthtrevino@gmail.com>
2023-03-24 14:49:49 -0700

    update libraries

---
+++ packages/test-stories/.storybook/manager.js
@@ -0,0 +1,8 @@
+import { configureManager } from '@essex/storybook-config/manager'
+
+configureManager({
+	themeVars: {
+		brandTitle: '@essex/storybook-test',
+		brandUrl: 'http://github.com/microsoft/essex-build'
+	}
+})
