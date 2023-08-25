be4c573d2e20f27c0b70a4d1b1ba06fac6d34c7e
Chris Trevino <chtrevin@microsoft.com>
2023-08-25 11:07:53 -0700

    test updates, lib updates

--- packages/test-webpack/src/index.tsx
+++ packages/test-webpack/src/index.tsx
@@ -7,5 +7,6 @@ import { createRoot } from 'react-dom/client'
 import { App } from './App.js'
 
 const rootElement = document.createElement('div')
+document.body.appendChild(rootElement)
 const root = createRoot(rootElement)
 root.render(<App />)
