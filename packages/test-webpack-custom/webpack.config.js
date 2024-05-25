9b44f24e804057ec6f92e341cc766e79d8f54548
Chris Trevino <chtrevin@microsoft.com>
2024-05-24 17:25:23 -0700

    use cjs in custom webpack app

--- packages/test-webpack-custom/webpack.config.js
+++ packages/test-webpack-custom/webpack.config.js
@@ -2,7 +2,7 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import essexWebpackConfig from '@essex/webpack-config'
+const essexWebpackConfig = require('@essex/webpack-config')
 
 const configuration = essexWebpackConfig.configure({
 	environment: (mode, env) => {
@@ -18,4 +18,4 @@ const configuration = essexWebpackConfig.configure({
 	},
 })
 
-export default configuration
+module.exports = configuration
